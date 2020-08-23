c31d2ee71067cdbe07818a0e877cdd5faf23bdb9
HouJie <houjie_jack@outlook.com>
2020-08-23 08:32:07 +0800

    增加封装异常捕获方法

---
+++ 03-JavaScript/基础/00-基本概念/try-catch.js
@@ -0,0 +1,29 @@
+/**
+ * 全局通用的同步try-catch方法
+ * @param {Function} asyncFunc 
+ */
+function tryCatchGlobal (asyncFunc) {
+  try {
+    return asyncFunc()
+  } catch (e) {
+    console.log(`同步异常捕捉_error:${e}`)
+  }
+}
+
+/**
+ * 全局通用的异步 promise-catch方法
+ * @param {Function} asyncFunc 异步方法
+ * @param {String} errMsg 
+ */
+function tryCatchGlobalPromise (asyncFunc, errMsg = "异步异常捕捉") {
+  return new Promise((resolve, reject) => {
+    // asyncFunc只能是异步方法,且必须返回一个Promise,
+    // 否则会报错【Promise才有then方法】
+    return asyncFunc().then((res) => {
+      resolve(res)
+    }).catch((error) => {
+      console.log(`${errMsg}_error:${error}：`)
+      resolve(null)
+    })
+  })
+}
