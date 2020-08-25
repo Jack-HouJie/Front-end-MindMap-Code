aa5b141b01d1c4b84dd6bd9792678a88d19e3d24
Jack-HouJie <47940738+Jack-HouJie@users.noreply.github.com>
2020-08-25 17:03:26 +0800

    整理二叉树代码

--- 03-JavaScript/数据结构与算法JS实现/03-树和二叉树/tree.js
+++ 03-JavaScript/数据结构与算法JS实现/03-树和二叉树/tree.js
@@ -10,6 +10,10 @@ class TNode {
   exec () {
     console.log(this.value);
   }
+  save (arr) {
+    arr.push(this.value)
+    console.log(arr);
+  }
 }
 class BiTree {
   constructor(node) {
@@ -199,7 +203,118 @@ class BiTree {
       }
     }
   }
+  /** 中序遍历树并保存
+   * 
+   * @param {TNode} node 开始遍历的节点
+   * @param {Array} result 存储遍历结果的数组 
+   */
+  saveMidOrder (node, result) {
+    if (!node) {
+      return null
+    }
+    this.saveMidOrder(node.left)
+    node.save(result)
+    this.saveMidOrder(node.right)
+  }
+  /** 找到第 k 小节点
+   * 
+   * @param {Number} k 
+   */
+  getKthMin (node, k) {
+    let result = []
+    tree.saveMidOrder(node, result)
+    console.log(result[k - 1]);
+  }
 }
+
+/** 二叉树遍历问题：
+ * 已知前中遍历，重建二叉树
+ * @param {String} pre "1,2,4,7,3,5,6,8"
+ * @param {String} mid "4,7,2,1,5,3,8,6"
+ */
+function reConstructBinaryTree (pre, mid) {
+  // 鲁棒性
+  if (!pre) {
+    return null
+  }
+  // 递归停止条件：
+  // 先序遍历队列剩最后一个
+  if (pre.length === 1) {
+    return new TNode(pre[0])
+  }
+  let curValue = pre[0]
+  let curIdx = mid.indexOf(curValue)
+  let leftMid = pre.slice(0, curIdx)
+  let rightMid = pre.slice(curIdx + 1)
+  let lfetPre = pre.slice(1, curIdx + 1)
+  let rightPre = pre.slice(curIdx + 1)
+  let node = new TNode(curValue)
+  node.left = reConstructBinaryTree(lfetPre, leftMid)
+  node.right = reConstructBinaryTree(rightPre, rightMid)
+  return node
+}
+
+/** 二叉树遍历问题：
+ * 已知前中遍历，求后序遍
+ * @param {String} pre 
+ * @param {String} mid 
+ */
+function getHRD (pre, mid) {
+  if (!pre) {
+    return ""
+  }
+  if (pre.length === 1) {
+    return pre[0]
+  }
+  let curNode = pre[0]
+  let curIdx = mid.indexOf(curNode)
+  let leftMid = mid.slice(0, curIdx)
+  let rightMid = mid.slice(curIdx + 1)
+  let leftPre = pre.slide(1, leftMid.length)
+  let rightPre = pre.slice(leftMid.length)
+  return getHRD(leftPre, leftMid) + getHRD(rightPre, rightMid) + curNode
+}
+
+/** 对称性问题：
+ * 判断二叉树是否对称
+ * @param {TNode} node1 
+ * @param {TNode} node2 
+ */
+function isSymmetrical (pRoot) {
+  if (!pRoot) {
+    return false
+  }
+  return isSymmetricalTree(pRoot, pRoot);
+}
+function isSymmetricalTree (node1, node2) {
+  if (!node1 && !node2) {
+    return true
+  }
+  if (!node1 || !node2) {
+    return false
+  }
+  if (node1.value !== node2.value) {
+    return false
+  }
+  return isSymmetricalTree(node1.left, node2.right) && isSymmetricalTree(node1.right, node2.left)
+}
+
+/** 对称性问题：
+ * 二叉树的镜像
+ * @param {TNode} root 
+ */
+function mirror (root) {
+  if (!root) {
+    return null
+  }
+  let rightVal = root.right.value
+  root.right.value = root.left.value
+  root.left.value = rightVal
+  mirror(root.left)
+  mirror(root.right)
+}
+
+
 
 let tree = new BiTree(new TNode(10))
 
@@ -214,4 +329,7 @@ tree.insert(tree.root, 105)
 // tree.midOrderNR(tree.root)
 // tree.backOrder(tree.root)
 // tree.breTravesal(tree.root)
-console.log(tree.getDeep(tree.root, 0));
+
+// console.log(tree.getDeep(tree.root, 0));
+
+tree.getKthMin(tree.root, 1)
