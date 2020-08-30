06c65a4df44ba585f2b9a9ed97fc0130422908bd
HouJie <houjie_jack@outlook.com>
2020-08-30 17:45:37 +0800

    整理代码

--- 02-JavaScript基础/03-引用类型/06-string.js
+++ 02-JavaScript基础/03-引用类型/06-string.js
@@ -1,15 +1,18 @@
 /* 应用 */
-// // 数组展开
-// let arr = [1, 1, [2, 3, [4]], "a", "b", ["c", "d"], "f", [["d"], "e"]];
-// console.log(arr.toString().split(','))
+let arr = [1, 1, [2, 3, [4]], "a", "b", ["c", "d"], "f", [["d"], "e"]];
+// 数组展开
+function zhankai(arr){
+  return arr.toString().split(',')
+}
+// console.log(arr)
 
-// // 判断回文
-// function valid (str) {
-//   if (typeof str !== 'string') {
-//     return false
-//   }
-//   return str.split('').reverse().join('') === str
-// }
+// 判断回文
+function valid (str) {
+  if (typeof str !== 'string') {
+    return false
+  }
+  return str.split('').reverse().join('') === str
+}
 // console.log(valid("1abcba"))
 
 /** 替换空格*
--- 02-JavaScript基础/06-BOM/BOM.js
+++ 02-JavaScript基础/06-BOM/BOM.js
@@ -1,58 +1,6 @@
-window.moveTo(200, 200);
-
-
-var leftPos =
-  typeof window.screenLeft == "number" ? window.screenLeft : window.screenX;
-var topPos =
-  typeof window.screenTop == "number" ? window.screenTop : window.screenY;
-
-console.log(leftPos);
-console.log(topPos);
-
-//取得视口大小
-var pageWidth = window.innerWidth,
-  pageHeight = window.innerHeight;
-if (typeof pageWidth != "number") {
-  if (document.compatMode == "CSS1Compat") {
-    pageWidth = document.documentElement.clientWidth;
-    pageHeight = document.documentElement.clientHeight;
-  } else {
-    pageWidth = document.body.clientWidth;
-    pageHeight = document.body.clientHeight;
-  }
-}
-
-// //改变窗口大小
-// window.resizeTo(100,500);
-
-// //超时调用实现间歇调用
-// var num = 0;
-// var max = 10;
-// function incrementNum(){
-//   num++;
-//   if(num < max){
-//     setTimeout(incrementNum,500);
-//   }else{
-//     console.log("done");
-//   }
-// }
-// setTimeout(incrementNum,500);
-
-// //对话框
-// if(confirm("is ok ?")){
-//   console.log("is ok");
-// }else{
-//   console.log("is not ok");
-// }
-
-// var username = prompt("Hi,who are you?", "enter your name here");
-// if (username) {
-//   console.log("welcome !" + username + "!");
-// }
-
-// 处理查询字符串
-// const q_str = location.search
-const q_str = 'name=zhangsan&age=14&school=s1&school=s2&student'
+/** 处理查询字符串
+ * 
+ */
 function getQueryString () {
   // 判断是否存在*，通过BOM得到查询字符串*
   // const q_str = location.search.length > 0 ? location.search.slice(1) : ''
@@ -79,11 +27,73 @@ function getQueryString () {
   console.log(map)
   return map
 }
+// const q_str = location.search
+const q_str = 'name=zhangsan&age=14&school=s1&school=s2&student'
 getQueryString()
 
+// 配置浏览器窗口
+function setView () {
+  // 获得窗口位置
+  var leftPos =
+    typeof window.screenLeft == "number" ? window.screenLeft : window.screenX;
+  var topPos =
+    typeof window.screenTop == "number" ? window.screenTop : window.screenY;
 
+  console.log(leftPos);
+  console.log(topPos);
+  // 移动窗口位置
+  window.moveTo(200, 200);
+  // 获得视口大小
+  var pageWidth = window.innerWidth,
+    pageHeight = window.innerHeight;
+  if (typeof pageWidth != "number") {
+    if (document.compatMode == "CSS1Compat") {
+      pageWidth = document.documentElement.clientWidth;
+      pageHeight = document.documentElement.clientHeight;
+    } else {
+      pageWidth = document.body.clientWidth;
+      pageHeight = document.body.clientHeight;
+    }
+  }
+  //改变窗口大小
+  window.resizeTo(100, 500);
+}
 
-// 检测浏览器插件
+// 超时调用实现间歇调用
+function myInterval () {
+  var num = 0;
+  var max = 10;
+  function incrementNum () {
+    num++;
+    if (num < max) {
+      setTimeout(incrementNum, 500);
+    } else {
+      console.log("done");
+    }
+  }
+  setTimeout(incrementNum, 500);
+}
+
+// 设置系统对话框
+function setLog () {
+  //对话框
+  if (confirm("is ok ?")) {
+    console.log("is ok");
+  } else {
+    console.log("is not ok");
+  }
+
+  var username = prompt("Hi,who are you?", "enter your name here");
+  if (username) {
+    console.log("welcome !" + username + "!");
+  }
+
+}
+
+/** 检测浏览器插件
+ * 
+ * @param {String} name 
+ */
 function hasPlugin (name) {
   name = name.toLowerCase();
   plu = navigator.plugins;
@@ -95,4 +105,7 @@ function hasPlugin (name) {
   }
   return false;
 }
+
+
+
 
