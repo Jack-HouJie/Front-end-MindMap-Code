4ed04520390fcf7a2c4aa3a4d007f43e4e40509f
HouJie <houjie_jack@outlook.com>
2020-08-30 15:18:22 +0800

    兼容性的添加事件函数

--- 02-JavaScript基础/08-事件/event.js
+++ 02-JavaScript基础/08-事件/event.js
@@ -1,4 +1,18 @@
-
+/** 视能力分别使用dom0||dom2||IE方式 来绑定事件
+ * 
+ * @param {Element} element 操作的元素
+ * @param {Strign} type 事件名称 'click'
+ * @param {Function} handler 事件处理程序
+ */
 function myAddEvent (element, type, handler) {
-
+  if (element.addEventListener) {
+    element.addEventListener(type, handler, false)
+  }
+  else if (element.attachEvent) {
+    element.attachEvent('on' + type, function () {
+      handler.call(element)
+    })
+  } else {
+    element['on' + type] = handler
+  }
 }
