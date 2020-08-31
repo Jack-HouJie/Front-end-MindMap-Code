b90d947117fa40aafe911a8bc361a1a67ed006bc
Jack-HouJie <47940738+Jack-HouJie@users.noreply.github.com>
2020-08-31 14:12:38 +0800

    规范变量名

--- 02-JavaScript基础/03-引用类型/01-object.js
+++ 02-JavaScript基础/03-引用类型/01-object.js
@@ -3,16 +3,17 @@
  */
 function myNew () {
   /** 1.创建一个新对象
-   *  2.将构造函数的作用域赋给新对象（因此this就指向了这个新对象）
+   *  2.将构造函数的作用域赋给新对象
+   * （因此this就指向了这个新对象）
    */
   // 取得指定构造函数（第一个参数）
-  let constr = Array.prototype.shift.call(arguments)
+  let constructor = Array.prototype.shift.call(arguments)
   // 创建空对象其原型为指定构造函数的原型
-  let obj = Object.create(constr.prototype)
+  let obj = Object.create(constructor.prototype)
   /** 3.为新对象添加属性:
    *  执行构造函数中的代码,
-   *  获取返回追 */
-  let result = constr.apply(obj, arguments)
+   *  获取返回值 */
+  let result = constructor.apply(obj, arguments)
   /** 4.如果构造函数有返回值，则返回；
    *  否则，就会默认返回新对象 */
   return result instanceof Object ? result : obj
@@ -23,10 +24,10 @@ function myNew () {
  * @param {Object} right 构造函数 
  */
 function myInstanceof (left, right) {
-  let right_prototype = right.prototype
+  let rightPrototype = right.prototype
   let left_prototype = left.__proto__
   while (left_prototype) {
-    if (left_prototype === right_prototype) {
+    if (left_prototype === rightPrototype) {
       return true
     }
     else {
