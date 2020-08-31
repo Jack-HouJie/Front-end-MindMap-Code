eaa46e9d2787ba691200c7011542a9335ca80aa1
Jack-HouJie <47940738+Jack-HouJie@users.noreply.github.com>
2020-08-31 11:16:41 +0800

    深拷贝注释

--- 03-JavaScript进阶/04-JSON/copy.js
+++ 03-JavaScript进阶/04-JSON/copy.js
@@ -1,26 +1,23 @@
 // // 浅拷贝
-// let a = {
-//   name: liming
-// }
-// let b = { ...a }
+let a = {
+  name: liming
+}
+let b = { ...a }
 
 // 深拷贝
-
-
-
+// 利用JSON序列化：
+// 会忽略 undefined
+// 会忽略 symbol
+// 不能序列化函数
+// 不能解决循环引用的对象
+// 可使用lodash的深拷贝函数解决
 let a = {
   user: {
     name: 'liming',
     age: 23
   }
 }
-
-// 局限性：
-// 会忽略 undefined
-// 不能序列化函数
-// 不能解决循环引用的对象
 let b = JSON.parse(JSON.stringify(a))
-
 b.user.name = 'zhangsan'
 console.log(b.user.name)
 console.log(a.user.name)
