5bd3411ab559f7dfb487abf69b2ce340e3273d26
Jack-HouJie <47940738+Jack-HouJie@users.noreply.github.com>
2020-09-08 13:50:13 +0800

    整理栈

--- 00-通识知识/01-数据结构与算法JS实现/02-栈和队列/01-栈/stack.js
+++ 00-通识知识/01-数据结构与算法JS实现/02-栈和队列/01-栈/stack.js
@@ -5,8 +5,7 @@ class MyStack {
     this.min = []
   }
   /** 入栈
-   * 
-   * @param {*}} item 
+   * @param {*} item 
    */
   push (item) {
     this.stack.push(item)
@@ -14,9 +13,9 @@ class MyStack {
       this.min[0] = item
     }
     else {
-      let cur_min = this.min[this.min.length - 1]
-      cur_min = item < cur_min ? item : cur_min
-      this.min.push(cur_min)
+      let curMin = this.min[this.min.length - 1]
+      curMin = item < curMin ? item : curMin
+      this.min.push(curMin)
     }
   }
   /** 出栈
@@ -95,9 +94,8 @@ let stack = new MyStack()
 // console.log(stack.getMin());
 
 
-/* 栈应用 */
-
-/**算术表达式问题：匹配有效括号
+/* 算术表达式问题 */
+/**匹配有效括号
  * 
  * https://leetcode-cn.com/problems/valid-parentheses/
  * 验证有效函数
@@ -132,13 +130,21 @@ function isValid (str) {
 }
 // console.log(isValid("()([{}])([}])"))
 
-/** 栈的压入弹出序列
+/* 压入弹出序列问题 */
+/** 栈的压入弹出序列*
  * 输入两个整数序列，第一个序列表示栈的压入顺序，
  * 请判断第二个序列是否可能为该栈的弹出顺序。
  * @param {Array} pushV 入栈序列
  * @param {Array} popV 出栈序列
+ * 
+ * 1.借助一个辅助栈来模拟压入、弹出的过程。
+ * 2.设置一个索引idx，记录popV（出栈序列）栈顶的位置
+ * 3.将pushV（压入顺序）中的数据依次入栈。
+ * 4.当辅助栈栈顶元素和压popV栈顶元素相同时，辅助栈出栈。每次出栈索引idx+1。
+ * 5.出栈有可能在任意一次入栈后进行，当辅助栈栈顶元素和压popV栈顶元素相同时，继续让pushV入辅助栈。
+ * 6.当所有数据入栈完成，如果出栈顺序正确，那么辅助栈应该为空。
  */
-function IsPopOrder (pushV, popV) {
+function isPopOrder (pushV, popV) {
   if (!pushV || !popV || pushV.length !== popV.length) {
     throw new Error('length error!')
   }
